1f82f72439269994982597d26fa21c67f593fa04
Cindy Carrillo <carrillo201994@gmail.com>
2023-08-25 18:13:43 -0500

    fix/refactoring products

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const { sequelize, products } = require("./models");
 
 const usersRouter = require("./routes/users");
 const postsRouter = require("./routes/posts");
+const productsRouter = require("./routes/products");
+// const ordersRouter = require("./routes/orders");
 
 //Users--------------
 app.use("/users", usersRouter);
@@ -14,71 +16,12 @@ app.use("/users", usersRouter);
 app.use("/posts", postsRouter);
 
 //Products------------
+app.use("/products", productsRouter);
 
-app.post("/products", async (req, res) => {
-  const {
-    productTitle,
-    productDescription,
-    productPrice,
-    productQty,
-    productImg,
-  } = req.body;
-
-  try {
-    const product = await products.create({
-      productTitle,
-      productDescription,
-      productPrice,
-      productQty,
-      productImg,
-    });
-    return res.json(product);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json(error);
-  }
-});
-
-app.get("/products", async (req, res) => {
-  try {
-    const productList = await products.findAll();
-    return res.json(productList);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ error: "Something went wrong" });
-  }
-});
-
-app.get("/products/:uuid", async (req, res) => {
-  const uuid = req.params.uuid;
-  try {
-    const product = await products.findOne({
-      where: { uuid },
-    });
-    return res.json(product);
-  } catch (error) {
-    console.log(error);
-    return res.status.json({ error: "Something went wrong" });
-  }
-});
-
-app.delete("/products/:uuid", async (req, res) => {
-  const uuid = req.params.uuid;
-  try {
-    const product = await products.findOne({
-      where: { uuid },
-    });
-    await product.destroy();
-
-    return res.json({ message: "Product deleted" });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ error: "Something went wrong" });
-  }
-});
+//Orders------------
+// app.use("/orders", ordersRouter);
 
 //Listen
-
 app.listen({ port: 5001 }, async () => {
   console.log("Server up on http://localhost:5001");
   await sequelize.authenticate();
