aee6d22d2cafcd42a24243ecf6172a3a542645b1
Cindy Carrillo <carrillo201994@gmail.com>
2023-09-21 22:25:06 -0500

    fix/migration import

--- run-migrations.js
+++ run-migrations.js
@@ -1,12 +1,12 @@
 const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL);
+const { execSync } = require("child_process"); // To execute CLI commands
 
 async function runMigrations() {
   try {
-    // Import your Sequelize models and migrations here
+    // Run Sequelize migrations using sequelize-cli
+    execSync("npx sequelize db:migrate", { stdio: "inherit" });
 
-    // Run the migrations
-    await sequelize.sync();
     console.log("Database migrations completed successfully.");
   } catch (error) {
     console.error("Error running migrations:", error);
