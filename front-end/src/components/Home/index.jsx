23bb274b7211bc86b4c0b9abb268bb16d1db2693
Cindy Carrillo <carrillo201994@gmail.com>
2024-03-12 21:27:31 -0500

    bug/cors error

--- front-end/src/components/Home/index.jsx
+++ front-end/src/components/Home/index.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { increment, decrement } from "../../redux/actions/example";
+import { getUsersData } from "../../redux/actions/users";
 
 export const Home = () => {
   const dispatch = useDispatch();
   const { count } = useSelector((state) => state.counter);
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    //Runs only on the first render
+
+    setUsers(dispatch(getUsersData()));
+    console.log("users", users);
+  }, []);
   return (
-    <div>
-      <p>Count: {count}</p>
-      <button onClick={() => dispatch(increment())}>Increment</button>
-      <button onClick={() => dispatch(decrement())}>Decrement</button>
-    </div>
+    <>
+      <div>
+        <p>Count: {count}</p>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+      </div>
+    </>
   );
 };
--- front-end/src/redux/actions/users/index.js
+++ front-end/src/redux/actions/users/index.js
@@ -1,6 +1,94 @@
-// import * as types from "front-end/src/redux/constants/users";
+// import * as types from "../constants/users";
+import * as types from "../../constants/users";
+import axios from "axios";
 
-// export const getUsers = (payload) => ({
-//   type: types.GET_USERS_REQUEST,
-//   payload,
+export const getUsersRequest = () => ({
+  type: types.GET_USERS_REQUEST,
+});
+
+export const getUsersSuccess = (data) => ({
+  type: types.GET_USERS_SUCCESS,
+});
+
+export const getUsersFailure = (error) => ({
+  type: types.GET_USERS_FAILURE,
+});
+
+export const getUsersData = () => {
+  return async (dispatch) => {
+    dispatch(getUsersRequest());
+    try {
+      //making http request to server
+      const response = await axios.get(
+        "https://ecommerceweb-emk3.onrender.com/users"
+      );
+      console.log("response", response);
+
+      //dispatching action to update redux store
+      dispatch(getUsersSuccess(response.data));
+    } catch (error) {
+      //dispatch the action to handle an error
+      console.log("error here");
+      dispatch(getUsersFailure(error.message));
+    }
+  };
+};
+// const fetchData = () => {
+//   return async (dispatch) => {
+//     dispatch(fetchDataRequest()); // Dispatch action to indicate data fetching has started
+
+//     try {
+//       // Make HTTP request to server
+//       const response = await axios.get('/api/data'); // Assuming your server API endpoint is '/api/data'
+
+//       // Dispatch action to update Redux store with fetched data
+//       dispatch(fetchDataSuccess(response.data));
+//     } catch (error) {
+//       // Dispatch action to handle error
+//       dispatch(fetchDataFailure(error.message));
+//     }
+//   };
+// };
+
+// // Import necessary dependencies
+// import axios from 'axios';
+
+// // Define action types
+// const FETCH_DATA_REQUEST = 'FETCH_DATA_REQUEST';
+// const FETCH_DATA_SUCCESS = 'FETCH_DATA_SUCCESS';
+// const FETCH_DATA_FAILURE = 'FETCH_DATA_FAILURE';
+
+// // Action creators
+// const fetchDataRequest = () => ({
+//   type: FETCH_DATA_REQUEST
 // });
+
+// const fetchDataSuccess = (data) => ({
+//   type: FETCH_DATA_SUCCESS,
+//   payload: data
+// });
+
+// const fetchDataFailure = (error) => ({
+//   type: FETCH_DATA_FAILURE,
+//   payload: error
+// });
+
+// // Asynchronous action creator using Redux Thunk
+// const fetchData = () => {
+//   return async (dispatch) => {
+//     dispatch(fetchDataRequest()); // Dispatch action to indicate data fetching has started
+
+//     try {
+//       // Make HTTP request to server
+//       const response = await axios.get('/api/data'); // Assuming your server API endpoint is '/api/data'
+
+//       // Dispatch action to update Redux store with fetched data
+//       dispatch(fetchDataSuccess(response.data));
+//     } catch (error) {
+//       // Dispatch action to handle error
+//       dispatch(fetchDataFailure(error.message));
+//     }
+//   };
+// };
+
+// export { fetchData };
