6f96971a4ed4c601acb14382b73976b7f1a98ddc
Mitchel Alejo <michalejo94@gmail.com>
2024-03-13 21:31:00 -0500

    Feat:Added Cors package and applied it to app.js

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
 app.use(express.json());
+app.use(cors());
+
+// app.use(cors({
+//   origin: "*",
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204,
+// }));
 
 const { sequelize } = require("./models");
 
--- front-end/src/redux/services/apiProvider.js
+++ front-end/src/redux/services/apiProvider.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useQuery } from "react-query";
+// import { useQuery } from "react-query";
 
 const baseURL = "https://ecommerceweb-emk3.onrender.com/";
 
@@ -11,14 +11,14 @@ const baseURL = "https://ecommerceweb-emk3.onrender.com/";
 //   },
 // });
 
-export function useGetUsers() {
-  return useQuery("users", async () => {
-    const response = await fetch(`${baseURL}/users`);
-    const data = await response.json();
-    console.log(data, "data");
-    return response.data; // Assuming the response contains user data
-  });
-}
+// export function useGetUsers() {
+//   return useQuery("users", async () => {
+//     const response = await fetch(`${baseURL}/users`);
+//     const data = await response.json();
+//     console.log(data, "data");
+//     return response.data; // Assuming the response contains user data
+//   });
+// }
 
 // export function useGetUsers() {
 //   return useQuery("users", async () => {
