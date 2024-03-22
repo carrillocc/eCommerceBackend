8abc0a1a2e63608d76562157fa1eaa268c81df39
Cindy Carrillo <carrillo201994@gmail.com>
2024-03-21 20:38:17 -0500

    fix/cors

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000" }));
 
 // app.use(cors({
 //   origin: "*",
