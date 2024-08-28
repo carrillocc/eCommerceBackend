12b7da43aa08fb9d040db9701066ed5e6a20fe3f
Cindy Carrillo <carrillo201994@gmail.com>
2024-08-27 19:45:41 -0500

    fix/antd home component

--- front-end/src/components/Home/index.jsx
+++ front-end/src/components/Home/index.jsx
@@ -1,35 +1,116 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUsers } from "../../redux/actions/users";
-import { Typography } from "antd";
+import { Breadcrumb, Layout, Menu, theme, Typography } from "antd";
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+
+const { Header, Content, Footer, Sider } = Layout;
+function getItem(label, key, icon, children) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  };
+}
+const items = [
+  getItem("Option 1", "1", <PieChartOutlined />),
+  getItem("Option 2", "2", <DesktopOutlined />),
+  getItem("User", "sub1", <UserOutlined />, [
+    getItem("Tom", "3"),
+    getItem("Bill", "4"),
+    getItem("Alex", "5"),
+  ]),
+  getItem("Team", "sub2", <TeamOutlined />, [
+    getItem("Team 1", "6"),
+    getItem("Team 2", "8"),
+  ]),
+  getItem("Files", "9", <FileOutlined />),
+];
 
 export const Home = () => {
   const dispatch = useDispatch();
   const { users, loading, errors } = useSelector((state) => state.users);
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
 
   useEffect(() => {
     dispatch(getUsers());
   }, []);
 
   return (
-    <>
-      <Typography.Title level={2}>Henlo World</Typography.Title>
-      {loading ? (
-        <p>Loading...</p>
-      ) : errors ? (
-        <p>Error: {errors}</p>
-      ) : (
-        <>
-          <Typography.Title level={3}>
-            Below is a list of users
-          </Typography.Title>
-          <ol>
-            {users.map((u) => {
-              return <li key={u.uuid}>{u.first_name}</li>;
-            })}
-          </ol>
-        </>
-      )}
-    </>
+    <Layout
+      style={{
+        minHeight: "100vh",
+      }}
+    >
+      <Sider
+        collapsible
+        collapsed={collapsed}
+        onCollapse={(value) => setCollapsed(value)}
+      >
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          defaultSelectedKeys={["1"]}
+          mode="inline"
+          items={items}
+        />
+      </Sider>
+      <Layout>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        />
+        <Content
+          style={{
+            margin: "0 16px",
+          }}
+        >
+          <Breadcrumb
+            style={{
+              margin: "16px 0",
+            }}
+          >
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            <Typography.Title level={3}>
+              Below is a list of users
+            </Typography.Title>
+            <ol>
+              {users.map((u) => {
+                return <li key={u.uuid}>{u.first_name}</li>;
+              })}
+            </ol>
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: "center",
+          }}
+        >
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
   );
 };
